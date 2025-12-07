5cd9447836806dac764854d6c490bd0756165a05
warungemung26 <warung.emung26@gmail.com>
2025-12-08 03:04:03 +0700

    Update file: js/data-loader.js

--- js/data-loader.js
+++ js/data-loader.js
@@ -1,3 +1,9 @@
+/*!
+ * Copyright (c) 2025, Atos
+ * All rights reserved.
+ * Unauthorized copying, modification, or distribution of this file is strictly prohibited.
+ */
+
 function shuffle(arr) {
   for (let i = arr.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
